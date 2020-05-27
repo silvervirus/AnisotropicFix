8396237075e69def80a0769fa36257d72c4872c3
M3dicCookie <pred102@hotmail.com>
2020-05-27 00:31:15 -0700

    uploaded source code

---
+++ AnisotropicFix/MainPatcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace AnisotropicFix
+{
+	// Token: 0x02000002 RID: 2
+	public class MainPatcher
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		public static void Patch()
+		{
+			try
+			{
+				MainPatcher.harmony = HarmonyInstance.Create("com.whotnt.subnautica.anisotropicfix.mod");
+				MainPatcher.harmony.Patch(AccessTools.Method(typeof(MainCameraControl), "Awake", null, null), null, new HarmonyMethod(typeof(MainPatcher).GetMethod("MCC_Awake_Postfix")), null);
+			}
+			catch (Exception ex)
+			{
+				Debug.Log("Error with Anisotropic Fix patching: " + ex.Message);
+			}
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x000020D4 File Offset: 0x000002D4
+		public static void MCC_Awake_Postfix()
+		{
+			QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+		}
+
+		// Token: 0x04000001 RID: 1
+		private static HarmonyInstance harmony;
+	}
+}
---
+++ AnisotropicFix/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("AnisotropicFix")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnisotropicFix")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d6b3b71a-9c91-473b-888e-9891657dd4f5")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
